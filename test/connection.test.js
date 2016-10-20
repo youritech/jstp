f08e09fe665099759bd39ec04b118635c1f3db68
Alexey Orlenko <eaglexrlnk@gmail.com>
2016-10-20 16:15:14 +0300

    Write anonymous handshake test
    * Implement an anonymous handshake test
    * Rename AbstractSocket#getPacketsFromBuffer to
      AbstractSocket#getBufferContent

--- test/connection.test.js
+++ test/connection.test.js
@@ -6,21 +6,98 @@ var chaiSpies = require('chai-spies');
 
 var Connection = require('..').Connection;
 
-var expect = chai.expect;  // eslint-disable-line no-unused-vars
+var expect = chai.expect;
 chai.use(chaiSpies);
 
 describe('JSTP Connection', function() {
+  // Transport mock
+  //
   var transportMock = new events.EventEmitter();
+  transportMock.buffer = '';
 
+  transportMock.getRemoteAddress = function() {
+    return '127.0.0.1';
+  };
+
+  transportMock.addDataToBuffer = function(data) {
+    if (data instanceof Buffer) {
+      data = data.toString();
+    }
+
+    transportMock.buffer += data;
+
+    if (data.endsWith('}')) {
+      transportMock.buffer += ',';
+    }
+  };
+
+  transportMock.isBufferReady = function() {
+    return transportMock.buffer.length > 0 &&
+      transportMock.buffer.endsWith(',');
+  };
+
+  transportMock.getBufferContent = function() {
+    return '[' + transportMock.buffer + ']';
+  };
+
+  transportMock.send = function(data) {  // eslint-disable-line no-unused-vars
+  };
+
+  transportMock.end = function(data) {
+    if (data) {
+      transportMock.send(data);
+    }
+  };
+
+  // Server mock
+  //
   var serverMock = new events.EventEmitter();
+
+  serverMock.startAuthenticatedSession = function(connection, application,
+      username, password, callback) {
+
+  };
+
+  serverMock.startAnonymousSession = function(connection, application,
+      callback) {
+
+  };
+
+  serverMock.getApplication = function(applicationName) {
+
+  };
 
   var clientMock = new events.EventEmitter();
 
-  var serverConnection;  // eslint-disable-line no-unused-vars
-  var clientConnection;  // eslint-disable-line no-unused-vars
+  // Client mock
+  //
+  clientMock.getApplication = function() {
+
+  };
+
+  var serverConnection;
+  var clientConnection;
 
   beforeEach(function() {
     serverConnection = new Connection(transportMock, serverMock);
     clientConnection = new Connection(transportMock, null, clientMock);
   });
+
+  it('should send anonymous handshake for the first time from a client',
+    function() {
+      chai.spy.on(transportMock, 'send');
+
+      var callback = chai.spy.on(function() {
+        transportMock.send.restore();
+      });
+
+      clientConnection.handshake('testApp', null, null, callback);
+      transportMock.emit('data',
+        '{handshake:[0],ok:\'12892e85-5bd7-4c77-a0c5-a0aecfcbc93a\'}');
+
+      expect(transportMock.send)
+        .to.be.called.with('{handshake:[0,\'testApp\']}');
+
+      expect(callback).to.be.called();
+    });
 });
