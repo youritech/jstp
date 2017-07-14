cfbb4e9e38e58bbeb6e2735c2505e225dbda4d3a
Dmytro Nechai <nechaido@gmail.com>
2017-07-14 15:47:18 +0300

    test: add handshake timeout test
    PR-URL: https://github.com/metarhia/jstp/pull/255
    Reviewed-By: Mykola Bilochub <nbelochub@gmail.com>
    Reviewed-By: Denys Otrishko <shishugi@gmail.com>

--- test/node/connection-handshake.js
+++ test/node/connection-handshake.js
@@ -126,3 +126,21 @@ test.test('must not accept handshakes on a client', (test) => {
   const connection = new jstp.Connection(transport, null, {});
   transport.emitPacket(handshake);
 });
+
+test.test(
+  'must drop connection after HANDSHAKE_TIMEOUT if no handshake recieved',
+  (test) => {
+    test.plan(3);
+    server.on('handshakeTimeout', () => {
+      test.pass('handshake timeout must occur');
+    });
+
+    const port = server.address().port;
+    net.connect(port, (error) => {
+      test.assertNot(error, 'must connect to server');
+      server.getClients()[0].on('close', () => {
+        test.pass('connection must be closed');
+      });
+    });
+  }
+);
