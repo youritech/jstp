a1ce409ed04e3c3ff60d805f23be6ed5de65c6ce
Dmytro Nechai <nechaido@gmail.com>
2017-07-31 19:03:55 +0300

    bench: add distributed benchmark
    PR-URL: https://github.com/metarhia/jstp/pull/292
    Reviewed-By: Denys Otrishko <shishugi@gmail.com>

--- benchmark/statistics.js
+++ benchmark/statistics.js
@@ -25,4 +25,51 @@ const stdev = (sample, meanValue) => {
   return Math.sqrt(variance);
 };
 
-module.exports = { mean, stdev };
+const combineCount = (samples) => {
+  let sum = 0;
+  for (let i = 0; i < samples.length; i++) {
+    sum += samples[i].count;
+  }
+  return sum;
+};
+
+const combineMean = (samples, count) => {
+  let sum = 0;
+  for (let i = 0; i < samples.length; i++) {
+    const sample = samples[i];
+    sum += sample.mean * sample.count;
+  }
+  return sum / count;
+};
+
+const combineStdev = (samples, mean, count) => {
+  let sum = 0;
+  for (let i = 0; i < samples.length; i++) {
+    const sample = samples[i];
+    sum += sample.count * Math.pow(sample.stdev, 2) +
+           sample.count * Math.pow(sample.mean - mean, 2);
+  }
+  return Math.sqrt(sum / count);
+};
+
+const combineSamples = (samples) => {
+  const len = samples.length;
+  if (len === 0)
+    return;
+  if (len === 1) {
+    return samples[0];
+  }
+  const count = combineCount(samples);
+  const mean  = combineMean(samples, count);
+  const stdev = combineStdev(samples, mean, count);
+  return { count, mean, stdev };
+};
+
+module.exports = {
+  mean,
+  stdev,
+  combineCount,
+  combineMean,
+  combineStdev,
+  combineSamples,
+};
