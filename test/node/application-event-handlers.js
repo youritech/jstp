bf26ce315dc9a150193a19bc0c2228ea919e52ff
Denys Otrishko <shishugi@gmail.com>
2017-08-01 16:27:00 +0300

    lib: allow to set event handlers in application
    PR-URL: https://github.com/metarhia/jstp/pull/286
    Reviewed-By: Dmytro Nechai <nechaido@gmail.com>
    Reviewed-By: Mykola Bilochub <nbelochub@gmail.com>

---
+++ test/node/application-event-handlers.js
@@ -0,0 +1,43 @@
+'use strict';
+
+const test = require('tap');
+
+const jstp = require('../..');
+
+const app = require('../fixtures/application');
+
+let server;
+let connection;
+
+test.afterEach((done) => {
+  if (connection) {
+    connection.close();
+    connection = null;
+  }
+  if (server) server.close();
+  done();
+});
+
+test.test('must call event handler in application on remote event', (test) => {
+  const expectedName = 'name';
+
+  const eventHandlers = {
+    someService: {
+      name(connection, name) {
+        test.assert(connection, 'must pass connection to event handler');
+        test.equals(name, expectedName, 'must pass correct argument');
+        test.end();
+      },
+    },
+  };
+  const application = new jstp.Application(app.name, {}, eventHandlers);
+  server = jstp.net.createServer([application]);
+  server.listen(0, () => {
+    const port = server.address().port;
+    jstp.net.connect(app.name, null, port, (error, conn) => {
+      connection = conn;
+      test.assertNot(error, 'must connect to server and perform handshake');
+      connection.emitRemoteEvent('someService', 'name', [expectedName]);
+    });
+  });
+});
