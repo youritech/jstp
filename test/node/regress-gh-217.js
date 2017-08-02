6fb5633d0a36ee4faa3c05d46e0bb96724990592
Alexey Orlenko <eaglexrlnk@gmail.com>
2017-08-02 21:32:51 +0300

    server: fix handshake timeout logic
    Clear handshake timeout on successful handshake and start sending
    heartbeat messages on successful handshake handler instead of handshake
    timeout handler.
    
    Fixes: https://github.com/metarhia/jstp/issues/217
    Fixes: https://github.com/metarhia/jstp/issues/283
    PR-URL: https://github.com/metarhia/jstp/pull/290
    Reviewed-By: Mykola Bilochub <nbelochub@gmail.com>
    Reviewed-By: Denys Otrishko <shishugi@gmail.com>
    Reviewed-By: Dmytro Nechai <nechaido@gmail.com>

---
+++ test/node/regress-gh-217.js
@@ -0,0 +1,38 @@
+// Regression test for https://github.com/metarhia/jstp/issues/217
+//
+// A confusing thing about this test is that it doesn't seem to have any
+// assertions except the one that tests if `jstp.net.connect()` has succeeded.
+// In fact, the crux of the test is that it just must complete.  When the
+// subject bug is present, it will crash with an unhandled exception.  This
+// exception happens in an asynchronous context, so `tap.doesNotThrow()` isn't
+// used here, tap will handle it top-level via a domain.
+
+'use strict';
+
+const tap = require('tap');
+const jstp = require('../..');
+
+const HANDSHAKE_TIMEOUT = 3000;
+
+const app = new jstp.Application('app', {});
+const server = jstp.net.createServer({
+  applications: [app],
+  heartbeatInterval: 100,
+});
+
+tap.plan(1);
+
+server.listen(() => {
+  jstp.net.connect(
+    'app', null, server.address().port,
+    (error, connection) => {
+      tap.assertNot(error, 'client must connect successfully');
+
+      connection.close();
+
+      setTimeout(() => {
+        server.close();
+      }, HANDSHAKE_TIMEOUT + 100);
+    }
+  );
+});
