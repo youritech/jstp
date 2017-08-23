32d4aaaf0841bfa90a909828ab01a1830050e79f
Dmytro Nechai <nechaido@gmail.com>
2017-08-23 20:27:05 +0300

    test: add test for resending on connection drop
    PR-URL: https://github.com/metarhia/jstp/pull/289
    Reviewed-By: Denys Otrishko <shishugi@gmail.com>
    Reviewed-By: Mykola Bilochub <nbelochub@gmail.com>

---
+++ test/node/session-resend-event-on-connection-drop.js
@@ -0,0 +1,70 @@
+'use strict';
+
+const test = require('tap');
+
+const cp = require('child_process');
+const path = require('path');
+
+const jstp = require('../..');
+
+let client = createClient();
+
+const iface = 'iface';
+const event = 'testEvent';
+const ifaces = {
+  iface: {
+    method: (connection, callback) => {
+      client.send(['sendSession']);
+      callback(null);
+    },
+  },
+};
+
+const application = new jstp.Application('testApp', ifaces);
+const serverConfig = { applications: [application] };
+const server = jstp.net.createServer(serverConfig);
+
+client.on('message', ([message, ...args]) => {
+  switch (message) {
+    case 'error':
+      console.error(args[0].message);
+      test.fail('must not encounter an error');
+      break;
+    case 'session':
+      session(...args);
+      break;
+  }
+});
+
+server.listen(0, () => {
+  client.send(['connect', server.address().port]);
+});
+
+function createClient() {
+  return cp.fork(path.join(__dirname, '../utils/session/client'));
+}
+
+function session(serializedSession) {
+  client.kill('SIGKILL');
+  server.getClientsArray()[0].emitRemoteEvent(iface, event, []);
+  client = createClient();
+  client.on('message', ([message, ...args]) => {
+    switch (message) {
+      case 'error':
+        console.error(args[0].message);
+        test.fail('must not encounter an error');
+        break;
+      case 'event':
+        handleEvent(...args);
+        break;
+    }
+  });
+  client.send(['reconnect', server.address().port, serializedSession]);
+}
+
+function handleEvent(ifaceName, eventName) {
+  test.equals(ifaceName, iface, 'interface name must match');
+  test.equals(eventName, event, 'event name must match');
+  client.kill();
+  server.close();
+}
