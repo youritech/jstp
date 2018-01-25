bfc22ef817f11b50abb8c42bd747f46beeee8788
Dmytro Nechai <nechaido@gmail.com>
2018-01-25 19:24:31 +0200

    lib: emit logging info from connection on a server
    PR-URL: https://github.com/metarhia/jstp/pull/312
    Reviewed-By: Mykola Bilochub <nbelochub@gmail.com>
    Reviewed-By: Denys Otrishko <shishugi@gmail.com>

---
+++ test/node/server-log.js
@@ -0,0 +1,20 @@
+'use strict';
+
+const test = require('tap');
+const jstp = require('../..');
+
+const app = new jstp.Application('app', {});
+const server = jstp.net.createServer({ applications: [app] });
+
+server.listen(() => {
+  server.once('log', () => {
+    test.pass('log event must be emmited');
+  });
+
+  jstp.net.connect('app', null, server.address().port, (err, connection) => {
+    test.assertNot(err, 'must connect successfully');
+
+    connection.close();
+    server.close();
+  });
+});
