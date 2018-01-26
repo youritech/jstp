bb15e4fc8e3924621de1d1db731d498333a42f07
Mykola Bilochub <nbelochub@gmail.com>
2018-01-26 13:45:08 +0200

    connection: fix remoteAddress being undefined
    PR-URL: https://github.com/metarhia/jstp/pull/313
    Reviewed-By: Dmytro Nechai <nechaido@gmail.com>
    Reviewed-By: Denys Otrishko <shishugi@gmail.com>

---
+++ test/node/regress-gh-313.js
@@ -0,0 +1,26 @@
+'use strict';
+
+const net = require('net');
+
+const test = require('tap');
+const jstp = require('../..');
+
+const app = new jstp.Application('app', {});
+const server = jstp.net.createServer({ applications: [app] });
+
+server.listen(() => {
+  jstp.net.connect(
+    'app', null, server.address().port,
+    (err, connection) => {
+      test.assertNot(err, 'must connect successfully');
+
+      test.assert(
+        net.isIP(connection.remoteAddress),
+        'remoteAddress must be an IP address'
+      );
+
+      connection.close();
+      server.close();
+    }
+  );
+});
