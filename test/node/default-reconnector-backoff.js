a150c9c61a35eded016f95d99a6408f742c34b85
Dmytro Nechai <nechaido@gmail.com>
2018-05-08 20:30:30 +0300

    test: add tests for reconnection
    PR-URL: https://github.com/metarhia/jstp/pull/335
    Reviewed-By: Mykola Bilochub <nbelochub@gmail.com>

---
+++ test/node/default-reconnector-backoff.js
@@ -0,0 +1,42 @@
+'use strict';
+
+const test = require('tap');
+
+const cp = require('child_process');
+const path = require('path');
+
+const jstp = require('../..');
+
+const APP_NAME = 'APP_NAME';
+
+const WAITING_TIME = 14000;
+const EXPECTED_ATTEMPTS_TO_RECONNECT = 4;
+
+const server =
+  cp.fork(path.join(__dirname, '../utils/reconnector/server-for-backoff.js'));
+
+server.on('message', ([type, port]) => {
+  test.plan(EXPECTED_ATTEMPTS_TO_RECONNECT + 1);
+
+  if (type !== 'listening') {
+    test.fail('must not recieve unknown messages');
+  }
+
+  jstp.net.connect(APP_NAME, null, port, 'localhost', (error, connection) => {
+    test.assertNot(error, 'must connect to server and perform handshake');
+
+    connection.on('error', () => {
+      // dismiss
+    });
+
+    server.kill('SIGTERM');
+
+    connection.on('reconnectAttempt', () => {
+      test.pass('must attempt to reconnect');
+    });
+
+    setTimeout(() => {
+      connection.close();
+    }, WAITING_TIME);
+  });
+});
---
+++ test/node/reconnection.js
@@ -0,0 +1,69 @@
+'use strict';
+
+const test = require('tap');
+
+const jstp = require('../..');
+
+const APP_NAME = 'APP_NAME';
+
+const TOKEN = 'TOKEN';
+
+const interfaces = {
+  iface: {
+    first(connection, token, callback) {
+      connection.session.set(TOKEN, token);
+      callback(null);
+    },
+
+    second(connection, callback) {
+      callback(null, connection.session.get(TOKEN));
+    },
+  },
+};
+
+const application = new jstp.Application(APP_NAME, interfaces);
+const serverConfig = { applications: [application] };
+
+let server;
+let connection;
+
+test.beforeEach((done) => {
+  server = jstp.net.createServer(serverConfig);
+  server.listen(0, () => {
+    const port = server.address().port;
+    jstp.net.connect(APP_NAME, null, port, 'localhost', (error, conn) => {
+      test.assertNot(error, 'must connect to server and perform handshake');
+      connection = conn;
+      done();
+    });
+  });
+});
+
+test.afterEach((done) => {
+  if (connection) {
+    connection.close();
+    connection = null;
+  }
+  server.close();
+  done();
+});
+
+test.test('must reconnect to the same session', (test) => {
+  connection.callMethod('iface', 'first', [TOKEN], (error) => {
+    test.assertNot(error, 'callMethod must not return an error');
+    connection.getTransport().destroy();
+    connection.on('error', () => {
+      // dismiss
+    });
+
+    setTimeout(() => {
+      connection.callMethod('iface', 'second', [], (error, token) => {
+        test.assertNot(error, 'callMethod must not return an error');
+        test.equal(token, TOKEN,
+          'second method must return the same token passed to first method'
+        );
+        test.end();
+      });
+    }, 1000);
+  });
+});
