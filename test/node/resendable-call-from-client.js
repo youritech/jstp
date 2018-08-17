6ad9a5ef0ded60ba086090594703ddcffe235d7c
Dmytro Nechai <nechaido@gmail.com>
2018-08-17 20:31:37 +0300

    test: simplify resendable call tests
    Fixes: https://github.com/metarhia/jstp/issues/361
    PR-URL: https://github.com/metarhia/jstp/pull/368
    Reviewed-By: Denys Otrishko <shishugi@gmail.com>
    Reviewed-By: Mykola Bilochub <nbelochub@gmail.com>

--- test/node/resendable-call-from-client.js
+++ test/node/resendable-call-from-client.js
@@ -2,123 +2,86 @@
 
 const test = require('tap');
 
-const cp = require('child_process');
-const path = require('path');
-
 const jstp = require('../..');
 
 const appName = 'application';
 
-const sessionStorage = new Map();
+let callsReceivedByServer = 0;
+const interfaces = {
+  iface: {
+    method: (connection, callback) => {
+      callsReceivedByServer++;
+      if (callsReceivedByServer === 1) {
+        test.pass('Server must receive first call message');
+        reconnect();
+      } else if (callsReceivedByServer === 2) {
+        test.pass('Server must receive second call message');
+        callback(null);
+      } else {
+        test.fail('Server must not receive more than two call messages');
+      }
+    },
+  },
+};
+
+let server;
+const sessionStorageProvider = new jstp.SimpleSessionStorageProvider();
 
 let connection;
 const client = {
   session: null,
 };
-let initialConnection = true;
-let callsReceivedByServer = 0;
 
-let server = createServer();
-
-function createServer() {
-  const server = cp.fork(
-    path.join(__dirname, '../utils/resendable-call/server.js')
-  );
-
-  server.on('message', ([message, ...args]) => {
-    switch (message) {
-      case 'error':
-        test.fail(args[0].message);
-        break;
-      case 'listening':
-        (initialConnection ? connect : reconnect)(args[0]);
-        break;
-      case 'getSession':
-        server.send([
-          'getSessionResponse',
-          args[0],
-          sessionStorage.get(args[0]),
-        ]);
-        break;
-      case 'setSession':
-        sessionStorage.set(args[0], args[1]);
-        if (initialConnection) {
-          call();
-        } else {
-          close();
-        }
-        break;
-      case 'callReceived':
-        if (++callsReceivedByServer !== 1) {
-          test.fail('server must receive only one call');
-        }
-        break;
-    }
+function createServer(callback) {
+  const application = new jstp.Application(appName, interfaces);
+  server = jstp.net.createServer({
+    applications: [application],
+    sessionStorageProvider,
   });
-
-  return server;
+  server.listen(0, () => {
+    callback(server.address().port);
+  });
 }
 
-let amountOfCalls = 0;
-function call() {
-  amountOfCalls++;
-  if (amountOfCalls !== 2) {
-    return;
-  }
-  connection.callMethodWithResend('iface', 'method', [], (error) => {
-    test.assertNot(error, 'must not encounter an error');
-
-    connection.close();
-    close();
-    test.end();
-  });
-
-  server.kill('SIGKILL');
-  server = createServer();
-}
-
-function connect(port) {
+function connect(port, callback) {
   jstp.net.connect(
     appName,
     client,
     port,
     (error, conn, session) => {
-      test.assertNot(error, 'must connect without errors');
-
+      test.error(error, 'must connect without errors');
       connection = conn;
-      connection.on('error', (error) => {
-        if (error.code !== 'ECONNRESET') {
-          test.fail('must not encounter errors other than `ECONNRESET`');
-        }
-      });
-
       client.session = session;
-      initialConnection = false;
-
-      call();
+      callback();
     }
   );
 }
 
-function reconnect(port) {
-  jstp.net.reconnect(connection, port, (error, conn) => {
-    test.assertNot(error, 'must connect without errors');
-
-    connection = conn;
-    connection.on('error', (error) => {
-      if (error.code !== 'ECONNRESET') {
-        test.fail('must not encounter errors other then `ECONNRESET`');
-      }
+function reconnect() {
+  connection.close();
+  connection.once('close', () => {
+    server.close();
+    createServer((port) => {
+      jstp.net.reconnect(connection, port, (error, conn) => {
+        test.error(error, 'must reconnect without errors');
+        connection = conn;
+      });
     });
   });
 }
 
-let calledClose = 0;
-function close() {
-  calledClose++;
-  if (calledClose !== 2) {
-    return;
-  }
-  server.send(['close']);
-  server.disconnect();
-}
+test.plan(6);
+
+createServer((port) => {
+  connect(
+    port,
+    () => {
+      connection.callMethodWithResend('iface', 'method', [], (error) => {
+        test.error(error, 'callMethodWithResend must not encounter an error');
+        test.pass('callback must be called');
+        connection.close();
+        server.close();
+      });
+    }
+  );
+});
--- test/node/resendable-call-from-server.js
+++ test/node/resendable-call-from-server.js
@@ -2,51 +2,77 @@
 
 const test = require('tap');
 
-const cp = require('child_process');
-const path = require('path');
-
 const jstp = require('../..');
 
-let client = createClient();
-
 const appName = 'application';
+
+let callsReceivedByClient = 0;
 const interfaces = {
   iface: {
-    method: (conn, callback) => {
-      callback(null);
+    method: (connection, callback) => {
+      callsReceivedByClient++;
+      if (callsReceivedByClient === 1) {
+        test.pass('Client must receive first call message');
+        reconnect();
+      } else if (callsReceivedByClient === 2) {
+        test.pass('Client must receive second call message');
+        callback(null);
+      } else {
+        test.fail('Client must not receive more than two call messages');
+      }
     },
   },
 };
 
-const application = new jstp.Application(appName, interfaces);
-const serverConfig = { applications: [application] };
-const server = jstp.net.createServer(serverConfig);
+let server;
+let port;
+let connection;
+const client = {
+  session: null,
+  application: new jstp.Application(appName, interfaces),
+};
 
-client.on('message', ([message, ...args]) => {
-  switch (message) {
-    case 'error':
-      test.fail(args[0].message);
-      break;
-    case 'connected':
-      client.kill('SIGKILL');
-      server
-        .getClientsArray()[0]
-        .callMethodWithResend('iface', 'method', [], (error) => {
-          test.assertNot(error, 'must not return an error');
-          server.close();
-          client.kill('SIGKILL');
-          test.end();
-        });
-      client = createClient();
-      client.send(['reconnect', server.address().port, args[0]]);
-      break;
-  }
-});
-
-server.listen(0, () => {
-  client.send(['connect', server.address().port]);
-});
-
-function createClient() {
-  return cp.fork(path.join(__dirname, '../utils/resendable-call/client.js'));
+function createServer(callback) {
+  const application = new jstp.Application(appName, interfaces);
+  server = jstp.net.createServer({ applications: [application] });
+  server.listen(0, () => {
+    port = server.address().port;
+    callback();
+  });
+  server.once('connect', (connection) => {
+    process.nextTick(() => {
+      connection.callMethodWithResend('iface', 'method', [], (error) => {
+        test.error(error, 'callMethodWithResend must not encounter an error');
+        test.pass('callback must be called');
+        server.close();
+        connection.close();
+      });
+    });
+  });
 }
+
+function connect() {
+  jstp.net.connect(
+    appName,
+    client,
+    port,
+    (error, conn, session) => {
+      test.error(error, 'must connect without errors');
+      connection = conn;
+      client.session = session;
+    }
+  );
+}
+
+function reconnect() {
+  connection.close();
+  connection.once('close', () => {
+    jstp.net.reconnect(connection, port, (error, conn) => {
+      test.error(error, 'must reconnect without errors');
+      connection = conn;
+    });
+  });
+}
+
+test.plan(6);
+createServer(connect);
