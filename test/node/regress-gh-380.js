d659e926b108d8d22c685d0728e73f3306b13689
Alexey Orlenko <eaglexrlnk@gmail.com>
2018-09-13 18:49:18 +0300

    test: add regression test for GH-380
    PR-URL: https://github.com/metarhia/jstp/pull/380
    Reviewed-By: Mykola Bilochub <nbelochub@gmail.com>

---
+++ test/node/regress-gh-380.js
@@ -0,0 +1,40 @@
+// Regression test for https://github.com/metarhia/jstp/pull/329
+
+'use strict';
+
+const test = require('tap');
+const { client: WebSocketClient } = require('websocket');
+
+const jstp = require('../..');
+
+const app = new jstp.Application('app', {});
+
+const makeTest = protocols => test => {
+  const server = jstp.ws.createServer([app]);
+
+  test.tearDown(() => {
+    server.close();
+  });
+
+  server.listen(0, () => {
+    const client = new WebSocketClient();
+    const url = `ws://localhost:${server.address().port}`;
+
+    client.once('connect', connection => {
+      connection.close();
+      test.fail('unreachable code: WS protocol negotiation is probably broken');
+      test.end();
+    });
+
+    client.once('connectFailed', () => {
+      test.end();
+    });
+
+    client.connect(url, protocols);
+  });
+};
+
+test.plan(2);
+
+test.test('must not crash with empty list of protocols', makeTest(null));
+test.test('must not crash with unsupported protocols', makeTest(['a', 'b']));
