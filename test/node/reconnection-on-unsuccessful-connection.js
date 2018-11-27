45fdb72bc465460c2aebe5c6c01c5d187a51515c
Mykola Bilochub <nbelochub@gmail.com>
2018-11-27 18:35:26 +0200

    conn: make reconnection consistent
    Completely disable reconnection before establishing a JSTP connection
    successfully for at least one time.
    
    Before, reconnection retries were implicitly happening before
    successfully establishing JSTP connection in a case when transport
    (tcp/tls/ws) connection was successfully established, but JSTP handshake
    failed. At the same time, reconnection was disabled whenever it was not
    possible to establish a transport connection to the server.
    
    PR-URL: https://github.com/metarhia/jstp/pull/386
    Reviewed-By: Denys Otrishko <shishugi@gmail.com>
    Reviewed-By: Dmytro Nechai <nechaido@gmail.com>

---
+++ test/node/reconnection-on-unsuccessful-connection.js
@@ -0,0 +1,52 @@
+'use strict';
+
+const test = require('tap');
+
+const jstp = require('../..');
+
+const TIMEOUT = 5000;
+
+const APP_NAME = 'APP_NAME';
+const INVALID_APP_NAME = 'INVALID_APP_NAME';
+
+const application = new jstp.Application(APP_NAME, {});
+const serverConfig = { applications: [application] };
+
+test.test('must not reconnect after failed connection', test => {
+  let reconnectionAttempts = 0;
+  const reconnector = () => {
+    reconnectionAttempts++;
+  };
+  jstp.net.connect(
+    APP_NAME, { reconnector }, 0, '__invalid_host__', error => {
+      test.assert(error, 'must fail handshake');
+    }
+  );
+  setTimeout(() => {
+    test.strictSame(reconnectionAttempts, 0);
+    test.end();
+  }, TIMEOUT);
+});
+
+test.test('must not reconnect after failed handshake', test => {
+  let connectionAttempts = 0;
+  const server = jstp.net.createServer(serverConfig);
+  server.on('connection', () => {
+    connectionAttempts++;
+  });
+  server.listen(0, () => {
+    const port = server.address().port;
+    jstp.net.connect(
+      INVALID_APP_NAME, null, port, 'localhost', error => {
+        test.strictSame(
+          error.code, jstp.ERR_APP_NOT_FOUND, 'must fail handshake'
+        );
+      }
+    );
+  });
+  setTimeout(() => {
+    test.strictSame(connectionAttempts, 1);
+    server.close();
+    test.end();
+  }, TIMEOUT);
+});
